9d40a09d94aedf59a63214bdef46254f1ede8579
certified-retart <baciziplatan78@gmail.com>
2025-05-19 12:15:05 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3515,11 +3515,14 @@ function Library:CreateWindow(...)
         local FadeTime = Config.MenuFadeTime;
         Fading = true;
         Toggled = (not Toggled);
-        ModalElement.Modal = Toggled;        
+        ModalElement.Modal = Toggled;
+
+        if getgenv().TWEAK_MOUSE_ICON then
+            InputService.MouseIconEnabled = Toggled;
+        end
 
         if Toggled then
             -- A bit scuffed, but if we're going from not toggled -> toggled we want to show the frame immediately so that the fade is visible.
-            InputService.MouseIconEnabled = true;
 
             Outer.Visible = true;
         end;
